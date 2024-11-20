875d097f8b9e2b6089ad1470025b83d9204350f2
pnayak770 <pintunayak770@gmail.com>
2024-11-20 18:31:45 +0530

    done the config part

---
+++ src/components/config/ConfigPanel.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./ConfigPanel.css"
+
+const ConfigPanel = ({ groupSize, itemCount, columns, setGroupSize, setItemCount, setColumns }) => {
+  return (
+    <div className="config-panel">
+      
+      <h2>Config</h2>
+      <div className="config-item">
+        <label>groupSize</label>
+        <input
+          type="range"
+          min="2"
+          max="4"
+          value={groupSize}
+          onChange={(e) => setGroupSize(Number(e.target.value))}
+        />
+        <span>{groupSize}</span>
+      </div>
+      <div className="config-item">
+        <label>itemCount</label>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={itemCount}
+          onChange={(e) => setItemCount(Number(e.target.value))}
+        />
+        <span>{itemCount}</span>
+      </div>
+      <div className="config-item">
+        <label>columns</label>
+        <input
+          type="range"
+          min="1"
+          max="6"
+          value={columns}
+          onChange={(e) => setColumns(Number(e.target.value))}
+        />
+        <span>{columns}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ConfigPanel;
